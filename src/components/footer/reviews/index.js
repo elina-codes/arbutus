484a2340925dcfc882b1cb46113ff0ab33680e3a
Elina <elinagoldin@gmail.com>
2021-08-11 20:52:18 -0700

    Add reviews widget

--- src/components/footer/reviews/index.js
+++ src/components/footer/reviews/index.js
@@ -1,5 +1,15 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+
 const Reviews = () => {
-  return null
+  return (
+    <>
+      <Helmet>
+        <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+      </Helmet>
+      <div class="elfsight-app-65367b6b-f563-4f85-996a-c0d1cda771c5"></div>
+    </>
+  )
 }
 
 export default Reviews
