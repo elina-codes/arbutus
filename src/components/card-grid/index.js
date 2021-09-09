56cc65a727fb75101fc8ff80ece76a89ea8643c0
Elina <elinagoldin@gmail.com>
2021-09-08 20:43:23 -0700

    Fix blog grid

--- src/components/card-grid/index.js
+++ src/components/card-grid/index.js
@@ -1,32 +1,39 @@
 import React from "react"
 import { Card, Text } from "components"
+import * as Mui from "@material-ui/core"
 
 const CardGrid = ({ data = [], leftAlign, ...cardProps }) => {
   let count = 0
   return (
-    <div style={leftAlign ? { textAlign: "left" } : null}>
+    <Mui.Grid
+      container
+      spacing={5}
+      justify={leftAlign ? "flex-start" : "center"}
+    >
       {data?.map((item, i) => {
         const { meta, title, text, image, url } = item || {}
         const delay = count * 100
         count = count > 1 ? 0 : count + 1
 
         return (
-          <Card
-            {...{
-              delay,
-              meta,
-              title,
-              image,
-              url,
-              key: `card-${i}-${title}`,
-              ...cardProps,
-            }}
-          >
-            <Text variant="body2">{text}</Text>
-          </Card>
+          <Mui.Grid item sm={6} md={4} key={`card-${i}-${title}`}>
+            <Card
+              {...{
+                delay,
+                meta,
+                title,
+                image,
+                url,
+                key: `card-${i}-${title}`,
+                ...cardProps,
+              }}
+            >
+              <Text variant="body2">{text}</Text>
+            </Card>
+          </Mui.Grid>
         )
       })}
-    </div>
+    </Mui.Grid>
   )
 }
 
